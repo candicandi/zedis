8f01a4248885899884e35c443bcdaf4dfc51d9be
Charles Fonseca <charles.fonseca@pm.me>
2025-11-18 00:57:25 -0300

    Fix debug build

--- src/main.zig
+++ src/main.zig
@@ -3,43 +3,8 @@ const Server = @import("server.zig");
 const config = @import("config.zig");
 const builtin = @import("builtin");
 
-const AllocatorContext = struct {
-    allocator: std.mem.Allocator,
-    gpa: if (builtin.mode == .Debug) std.heap.DebugAllocator(.{
-        .stack_trace_frames = 10,
-        .safety = true,
-        .thread_safe = true,
-    }) else void,
-
-    fn init() AllocatorContext {
-        if (builtin.mode == .Debug) {
-            var ctx: AllocatorContext = undefined;
-            ctx.gpa = std.heap.DebugAllocator(.{
-                .stack_trace_frames = 10,
-                .safety = true,
-                .thread_safe = true,
-            }){};
-            ctx.allocator = ctx.gpa.allocator();
-            return ctx;
-        } else {
-            return .{
-                .allocator = std.heap.page_allocator,
-                .gpa = {},
-            };
-        }
-    }
-
-    fn deinit(self: *AllocatorContext) void {
-        if (builtin.mode == .Debug) {
-            _ = self.gpa.deinit();
-        }
-    }
-};
-
 pub fn main() !void {
-    var alloc_ctx = AllocatorContext.init();
-    defer alloc_ctx.deinit();
-    const allocator = alloc_ctx.allocator;
+    const allocator = std.heap.page_allocator;
 
     const cfg = config.readConfig(allocator) catch |err| {
         std.log.err("Error reading config: {s}", .{@errorName(err)});
