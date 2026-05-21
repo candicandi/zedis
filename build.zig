27cd5916ce98ab5923e7454375b52d895ecf0926
Charles Fonseca <charles.fonseca@pm.me>
2026-05-21 18:01:20 -0300

    fix: change optimization level to ReleaseSafe for consistent builds

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSafe });
 
     const zio = b.dependency("zio", .{ .target = target, .optimize = optimize });
     const zio_mod = zio.module("zio");
